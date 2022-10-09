7d2df7907ebc563a7f99f781c6c6b6bed6c1d924
boonlertChin <matrohero@gmail.com>
2022-10-09 14:07:19 +0700

    1 hello

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/boonlertChin/golang_bf
+
+go 1.19
---
+++ golang_bf.go
@@ -0,0 +1,11 @@
+// go mod init github.com/boonlertChin/golang_bf
+package golang_bf
+
+type testConstrant struct {
+	Test string
+	Pass string
+}
+
+func TestPrintConstrant() string {
+	return "Hello"
+}
